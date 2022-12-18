a27287a8fb342ab6717e1c93eb0bf4eba1a51705
Jurnell Cockhren <jcockhren@users.noreply.github.com>
2022-12-17 18:09:54 -0600

    Check the in-house schemas (#3)
    * firstly, move all dev packages to the dev section
    
    * fixup testing suite to check the in-house schemas
    
    * circleCI config

--- tests.py
+++ tests.py
@@ -3,11 +3,12 @@ import json
 
 import pytest
 
-from jsonschema import Draft7Validator
+from jsonschema import Draft7Validator, validators
 import jsonschema
 from stix2validator import validate_file, print_results, ValidationOptions
 
 EXTENSION_DIRECTORY = 'stix-extensions'
+SCHEMA_DIRECTORY = 'schemas'
 
 
 @pytest.mark.parametrize("example", [f.name for f in Path(EXTENSION_DIRECTORY).iterdir() if f.is_file()])
@@ -33,3 +34,12 @@ def test_json_examples(scenario):
         instance = json.load(i)
         v = jsonschema.validate(instance, schema)
     assert True
+
+
+@pytest.mark.schemas
+@pytest.mark.parametrize("schema_under_test", [f.name for f in Path(SCHEMA_DIRECTORY).iterdir() if f.is_file()])
+def test_validate_in_house_schemas(schema_under_test):
+    s_path = f'{SCHEMA_DIRECTORY}/{schema_under_test}'
+    with open(s_path) as s:
+        schema = json.load(s)
+        assert validators.Draft202012Validator.check_schema(schema) is None
