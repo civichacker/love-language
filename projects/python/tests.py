46dcdc11f6b7d674eba321d938dbd5aa2c58d094
Jurnell Cockhren <jurnell@civichacker.com>
2023-06-10 16:35:20 -0500

    be sure that schema will still convert to a dictionary

--- projects/python/tests.py
+++ projects/python/tests.py
@@ -1,6 +1,10 @@
 import importlib
+import json
 
 
 def test_use_of_force_import():
     lib = importlib.import_module('lovelanguage')
     assert hasattr(lib, 'USEOFFORCE')
+    USEOFFORCE = getattr(lib, 'USEOFFORCE')
+    force = json.loads(USEOFFORCE)
+    assert 'force' in force.get('title')
